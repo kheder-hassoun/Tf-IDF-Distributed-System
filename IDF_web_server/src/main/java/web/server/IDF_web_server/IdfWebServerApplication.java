a817250f42cec627b5c92b6c55326bac1ac79960
kheder <khdrhswn32@gmail.com>
2025-01-22 02:45:25 +0300

    add web project

---
+++ IDF_web_server/src/main/java/web/server/IDF_web_server/IdfWebServerApplication.java
@@ -0,0 +1,13 @@
+package web.server.IDF_web_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdfWebServerApplication.class, args);
+	}
+
+}
