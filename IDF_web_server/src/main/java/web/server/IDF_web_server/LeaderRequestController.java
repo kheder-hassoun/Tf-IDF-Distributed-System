e4dec56ff4a8ee7ae87acfed841f7bd12b65c7d7
kheder <khdrhswn32@gmail.com>
2025-01-22 21:20:57 +0300

    Add Leader_info ZNode

--- IDF_web_server/src/main/java/web/server/IDF_web_server/LeaderRequestController.java
+++ IDF_web_server/src/main/java/web/server/IDF_web_server/LeaderRequestController.java
@@ -1,8 +1,8 @@
 package web.server.IDF_web_server;
 
-
-
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,26 +13,34 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class LeaderRequestController {
 
-    @Value("${leader.url}")
-    private String leaderUrl;
-
+    private final ZooKeeper zooKeeper;
     private final RestTemplate restTemplate;
+    private static final String LEADER_INFO_PATH = "/leader_info";
 
-    public LeaderRequestController() {
+    @Autowired
+    public LeaderRequestController(ZooKeeper zooKeeper) {
+        this.zooKeeper = zooKeeper;
         this.restTemplate = new RestTemplate();
     }
 
     @GetMapping("/search")
     public ResponseEntity<String> handleSearchRequest(@RequestParam String query) {
         try {
+            // Retrieve the leader's port from the ZooKeeper znode
+            String leaderPort = new String(zooKeeper.getData(LEADER_INFO_PATH, false, null));
+
+            // Construct the leader's URL dynamically
+            String leaderUrl = String.format("http://localhost:%s/leader/start", leaderPort);
+
             // Forward the search query as a POST request to the leader
-            RestTemplate restTemplate = new RestTemplate();
             HttpEntity<String> requestEntity = new HttpEntity<>(query);
-            String response = restTemplate.postForObject(leaderUrl , requestEntity, String.class);
+            String response = restTemplate.postForObject(leaderUrl, requestEntity, String.class);
+
             return ResponseEntity.ok(response);
+        } catch (KeeperException | InterruptedException e) {
+            return ResponseEntity.status(500).body("Error retrieving leader information: " + e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(500).body("Error communicating with leader: " + e.getMessage());
         }
     }
-
 }
--- TF-IDF-System-Core/src/main/java/me/zookeeper/leader_election/OnElectionAction.java
+++ TF-IDF-System-Core/src/main/java/me/zookeeper/leader_election/OnElectionAction.java
@@ -1,44 +1,71 @@
 package me.zookeeper.leader_election;
 
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+
 @Component
 public class OnElectionAction implements OnElectionCallback {
 
-
     private final ServiceRegistry serviceRegistry;
-    private final int port;
+    private final ZooKeeper zooKeeper;
+    private final Environment environment; // Inject Spring Environment
+
     @Autowired
-    public OnElectionAction(ServiceRegistry serviceRegistry, @Value("${election.port}") int port) {
+    public OnElectionAction(ServiceRegistry serviceRegistry, ZooKeeper zooKeeper, Environment environment) {
         this.serviceRegistry = serviceRegistry;
-        this.port = port;
+        this.zooKeeper = zooKeeper;
+        this.environment = environment;
     }
 
     @Override
     public void onElectedToBeLeader() {
         serviceRegistry.unregisterFromCluster();
         serviceRegistry.registerForUpdates();
+
+        String leaderInfoPath = "/leader_info";
+
+        try {
+            // Get the current server port dynamically
+            int runningPort = Integer.parseInt(environment.getProperty("local.server.port"));
+
+            String leaderInfoData = String.valueOf(runningPort);
+
+            // Create the znode if it doesn't exist, or update it if it does
+            if (zooKeeper.exists(leaderInfoPath, false) == null) {
+                zooKeeper.create(
+                        leaderInfoPath,
+                        leaderInfoData.getBytes(),
+                        ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                        CreateMode.EPHEMERAL
+                );
+            } else {
+                zooKeeper.setData(leaderInfoPath, leaderInfoData.getBytes(), -1);
+            }
+        } catch (KeeperException | InterruptedException | NumberFormatException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onWorker() {
         try {
+            // Get the current server port dynamically
+            int runningPort = Integer.parseInt(environment.getProperty("local.server.port"));
+
             String currentServerAddress =
-                    String.format("http://%s:%d", InetAddress.getLocalHost().getCanonicalHostName(), port);
+                    String.format("http://%s:%d", InetAddress.getLocalHost().getCanonicalHostName(), runningPort);
 
             serviceRegistry.registerToCluster(currentServerAddress);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (KeeperException e) {
+        } catch (InterruptedException | UnknownHostException | KeeperException | NumberFormatException e) {
             e.printStackTrace();
         }
-
     }
 }
